f775745526c0beebf43ff60c92dd58170a5ac015
jfkeci <ja.lip132@gmail.com>
2022-03-20 21:10:41 +0100

    feat: server side auth

---
+++ server/resources/auth/auth.controller.js
@@ -0,0 +1,63 @@
+import { connect } from 'getstream';
+import bcrypt from 'bcrypt';
+import StreamChat from 'stream-chat';
+import crypto from 'crypto';
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+export const login = async (req, res) => {
+  try {
+    const { fullName, username, password, phoneNumber } = req.body;
+
+    const userId = crypto.randomBytes(16).toString('hex');
+
+    const serverClient = connect(api_key, api_secret, app_id);
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const token = serverClient.createUserToken(userId);
+
+    res.status(200).json({
+      token,
+      fullname,
+      username,
+      userId,
+      hash,
+      phoneNumber
+    })
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+export const signup = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const serverClient = connect(api_key, api_secret, app_id);
+
+    const client = StreamChat.getInstance(api_key, api_secret);
+
+    const { users } = await client.queryUsers({ name: username });
+
+    if (!users.length) return res.status(409).send('Invalid credentials')
+
+    const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+    const token = serverClient.createUserToken(users[0].id);
+
+    if (success) res.status(200).json({
+      token,
+      fullName: users[0].fullName,
+      username: users[0].username,
+      userId: users[0].userId,
+    })
+
+    return res.status(409).send('Invalid credentials')
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
