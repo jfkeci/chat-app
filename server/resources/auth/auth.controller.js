3028ae550e6a9264193fbc2806a773cb5aed8c58
jfkeci <ja.lip132@gmail.com>
2022-03-21 07:31:04 +0100

    chore: refactor auth

--- server/resources/auth/auth.controller.js
+++ server/resources/auth/auth.controller.js
@@ -1,11 +1,16 @@
-import { connect } from 'getstream';
-import bcrypt from 'bcrypt'
+import stream from 'getstream';
+import bcrypt from 'bcrypt';
 import { StreamChat } from 'stream-chat';
 import crypto from 'crypto';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
+
+
 
 export const signup = async (req, res) => {
   try {
@@ -13,24 +18,17 @@ export const signup = async (req, res) => {
 
     const userId = crypto.randomBytes(16).toString('hex');
 
-    const serverClient = connect(api_key, api_secret, app_id);
+    const serverClient = stream.connect(api_key, api_secret, app_id);
 
-    const hash = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const token = serverClient.createUserToken(userId);
 
-    return res.status(200).json({
-      token,
-      fullName,
-      username,
-      userId,
-      hash,
-      phoneNumber
-    });
-
+    res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: error });
+    console.log(error);
+
+    res.status(500).json({ message: error });
   }
 };
 
@@ -38,28 +36,26 @@ export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const serverClient = connect(api_key, api_secret, app_id);
-
+    const serverClient = stream.connect(api_key, api_secret, app_id);
     const client = StreamChat.getInstance(api_key, api_secret);
 
     const { users } = await client.queryUsers({ name: username });
 
-    if (!users.length) return res.status(409).json({ message: 'Invalid credentials' });
+    if (!users.length) return res.status(400).json({ message: 'User not found' });
 
     const success = await bcrypt.compare(password, users[0].hashedPassword);
 
     const token = serverClient.createUserToken(users[0].id);
 
-    if (success) return res.status(200).json({
-      token,
-      fullName: users[0].fullName,
-      username,
-      userId: users[0].id
-    });
-
-    return res.status(409).json({ message: 'Invalid credentials' });
+    if (success) {
+      res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id });
+    } else {
+      res.status(500).json({ message: 'Incorrect password' });
+    }
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: error });
+    ads
+    console.log(error);
+
+    res.status(500).json({ message: error });
   }
 };
